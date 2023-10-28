e7d62b82a1eb65db784f8577f175b354bf71a1bd
JunHyuk <106327540+Junnnhhh@users.noreply.github.com>
2023-10-28 11:30:37 +0900

    Create 숫자의 표현.java

---
+++ Programmers/Lv.2/숫자의 표현.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i=1; i<=n; i++) {
+            int num = i;
+            int sum = 0;
+            
+            while(sum < n) {
+                sum += num;
+                num++;
+            }
+            
+            if(sum == n) {
+                answer++;  
+            } 
+        }
+        
+        return answer;
+    }
+}
